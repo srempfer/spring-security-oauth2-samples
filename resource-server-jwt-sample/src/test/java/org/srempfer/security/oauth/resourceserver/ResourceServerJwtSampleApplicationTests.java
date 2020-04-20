8a9f4a44ae0b72e9c5c07fc82f2962482ceda5c1
Stefan Rempfer <rempfer@gmail.com>
2020-04-20 21:10:26 +0200

    Added an resource server sample with JWT Token support

---
+++ resource-server-jwt-sample/src/test/java/org/srempfer/security/oauth/resourceserver/ResourceServerJwtSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.srempfer.security.oauth.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerJwtSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
